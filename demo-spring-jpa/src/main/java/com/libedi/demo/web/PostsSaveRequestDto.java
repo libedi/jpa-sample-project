f07e00252235d6df54dda6e536fe8802f64af0d1
Sangjun, Park <libedi@gmail.com>
2019-01-08 11:35:53 +0900

    [demo-spring-jpa] LocalDateTime을 사용하기 위한 @DateTimeFormat 애노테이션 추가

--- demo-spring-jpa/src/main/java/com/libedi/demo/web/PostsSaveRequestDto.java
+++ demo-spring-jpa/src/main/java/com/libedi/demo/web/PostsSaveRequestDto.java
@@ -1,18 +1,29 @@
 package com.libedi.demo.web;
 
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.libedi.demo.domain.posts.Posts;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter @Setter
 @NoArgsConstructor
+@ToString
 public class PostsSaveRequestDto {
 
     private String title;
     private String content;
     private String author;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")    /* RequestBody일 경우 필요 */
+    private LocalDateTime time;
 
     public Posts toEntity() {
         return Posts.builder().title(title).content(content).author(author).build();
--- demo-spring-jpa/src/main/java/com/libedi/demo/web/WebRestController.java
+++ demo-spring-jpa/src/main/java/com/libedi/demo/web/WebRestController.java
@@ -2,16 +2,17 @@ package com.libedi.demo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.libedi.demo.domain.posts.Posts;
 import com.libedi.demo.domain.posts.PostsRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class WebRestController {
 
     private final PostsRepository postsRepository;
@@ -22,7 +23,8 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody final PostsSaveRequestDto dto) {
+    public void savePosts(final PostsSaveRequestDto dto) {
+        log.info(dto.toString());
         Posts posts = postsRepository.save(dto.toEntity());
         posts.setTitle("수정된 타이틀");
         postsRepository.flush();
